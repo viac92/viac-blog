fcab501562fa5460fe79a2ad5f3f12ffb4576c05
viac92 <64146594+viac92@users.noreply.github.com>
2023-09-21 12:46:31 +0200

    add home button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-// import BlogPage from './views/BlogPage.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function goToHome() {
+  router.push("/");
+}
 
 </script>
 
 <template>
+  <div @click="goToHome">
+    <h1>Home Page</h1>
+  </div>
   <Suspense>
     <router-view />
   </Suspense>
