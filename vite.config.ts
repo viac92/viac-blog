beb789473244b337f5357d3dcf640b5401b289d2
viac92 <64146594+viac92@users.noreply.github.com>
2023-09-21 09:12:50 +0200

    aggiunto supporto esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +13,21 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        nodeModulesPolyfillPlugin({
+          globals: {
+            process: true,
+            Buffer: true
+          }
+        })
+      ]
     }
   }
 })
