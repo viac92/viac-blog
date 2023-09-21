c5568c71c40d8ab12f26885e6f8473a063b20764
viac92 <64146594+viac92@users.noreply.github.com>
2023-09-21 09:16:48 +0200

    add blog post

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import matter from 'gray-matter';
+import { ref } from 'vue';
+import { marked } from 'marked';
+
+const props = defineProps({
+    content: {
+        type: String,
+        required: true
+    }
+})
+
+const matterResult = ref(matter(props.content));
+const html = ref(marked.parse(matterResult.value.content));
+
+</script>
+<template>
+    <h1>{{ matterResult.data.title }}</h1>
+    <p>{{ matterResult.data.date }}</p>
+    <div v-html="html"></div>
+</template>
