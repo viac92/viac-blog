7117ebc8833265242b73f40f4af5f66dee9bc234
viac92 <64146594+viac92@users.noreply.github.com>
2023-09-21 12:45:57 +0200

    better polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    nodePolyfills({
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-      plugins: [
-        nodeModulesPolyfillPlugin({
-          globals: {
-            process: true,
-            Buffer: true
-          }
-        })
-      ]
-    }
-  }
 })
