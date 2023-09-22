723d4d0ecb66f19293e083a785b2a22e75dd1c5e
viac92 <64146594+viac92@users.noreply.github.com>
2023-09-22 11:29:08 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import PostHomeViewer from '@/components/PostHomeViewer.vue'
+</script>
+<template>
+    <div 
+        class="
+            flex 
+            flex-col 
+            items-center 
+            bg-repeat 
+            background
+        "
+    >
+        <PostHomeViewer />
+    </div>
+</template>
