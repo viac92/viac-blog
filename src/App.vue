e457e7e96c54f6bde6765d645cfaec415b32a0c4
viac92 <64146594+viac92@users.noreply.github.com>
2023-09-22 11:26:10 +0200

    move layout away from app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-function goToHome() {
-  router.push("/");
-}
-
 </script>
-
 <template>
-  <div class="flex flex-col items-center bg-cover bg-repeat background">
-    <div @click="goToHome" class="p-8 hover:cursor-pointer">
-      <h1 class="text-5xl">VIAC's blog</h1>
-    </div>
     <Suspense>
       <router-view />
     </Suspense>
-  </div>
 </template>
